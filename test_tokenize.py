0a3baefbab53dc3c47d061e74840eadadd5a70e7
arsh23 <programmer.arsh@gmail.com>
2018-06-21 03:08:47 +0530

    tokenizer v4 and tests v2

--- test_tokenize.py
+++ test_tokenize.py
@@ -1,22 +1,27 @@
 import unittest
-from tokenizer import tokenize
+from tokenizer import Token, tokenize
 
 
 class TestTokenize(unittest.TestCase):
 
     def test_empty(self):
         text = '{}'
-        output = [('OPEN_CURLYBRACE', '{'), ('CLOSE_CURLYBRACE', '}')]
-        self.assertEqual(tokenize(text), output)
+        output = [
+            Token(name='OPEN_CURLYBRACE', value='{', line=1),
+            Token(name='CLOSE_CURLYBRACE', value='}', line=1)
+        ]
+        self.assertEqual(list(tokenize(text)), output)
 
     def test_single(self):
         text = '{"key1": "value1"}'
-        output = [('OPEN_CURLYBRACE', '{'),
-                  ('KEY', 'key1'),
-                  ('COLON', ':'),
-                  ('STRING', 'value1'),
-                  ('CLOSE_CURLYBRACE', '}')]
-        self.assertEqual(tokenize(text), output)
+        output = [
+            Token(name='OPEN_CURLYBRACE', value='{', line=1),
+            Token(name='KEY', value='key1', line=1),
+            Token(name='COLON', value=':', line=1),
+            Token(name='STRING', value='value1', line=1),
+            Token(name='CLOSE_CURLYBRACE', value='}', line=1)
+        ]
+        self.assertEqual(list(tokenize(text)), output)
 
     def test_multiple(self):
         text = '''{
@@ -30,48 +35,83 @@ class TestTokenize(unittest.TestCase):
             ],
             "la\"st\\": -1234.123123e+123123
         }'''
-        output = [('OPEN_CURLYBRACE', '{'),
-                  ('KEY', 'str'),
-                  ('COLON', ':'),
-                  ('STRING', 'value1'),
-                  ('COMMA', ','),
-                  ('KEY', 'num'),
-                  ('COLON', ':'),
-                  ('NUMBER', '2'),
-                  ('COMMA', ','),
-                  ('KEY', 't'),
-                  ('COLON', ':'),
-                  ('TRUE', 'true'),
-                  ('COMMA', ','),
-                  ('KEY', 'f'),
-                  ('COLON', ':'),
-                  ('FALSE', 'false'),
-                  ('COMMA', ','),
-                  ('KEY', 'n'),
-                  ('COLON', ':'),
-                  ('NULL', 'null'),
-                  ('COMMA', ','),
-                  ('KEY', 'list'),
-                  ('COLON', ':'),
-                  ('OPEN_SQBRACE', '['),
-                  ('STRING', 'text'),
-                  ('COMMA', ','),
-                  ('NUMBER', '1'),
-                  ('COMMA', ','),
-                  ('TRUE', 'true'),
-                  ('COMMA', ','),
-                  ('OPEN_CURLYBRACE', '{'),
-                  ('KEY', 'nested'),
-                  ('COLON', ':'),
-                  ('STRING', 'inside'),
-                  ('CLOSE_CURLYBRACE', '}'),
-                  ('CLOSE_SQBRACE', ']'),
-                  ('COMMA', ','),
-                  ('KEY', 'la\"st\\'),
-                  ('COLON', ':'),
-                  ('NUMBER', '-1234.123123e+123123'),
-                  ('CLOSE_CURLYBRACE', '}')]
-        self.assertEqual(tokenize(text), output)
+        output = [
+            Token(name='OPEN_CURLYBRACE', value='{', line=1),
+            Token(name='KEY', value='str', line=2),
+            Token(name='COLON', value=':', line=2),
+            Token(name='STRING', value='value1', line=2),
+            Token(name='COMMA', value=',', line=2),
+            Token(name='KEY', value='num', line=3),
+            Token(name='COLON', value=':', line=3),
+            Token(name='NUMBER', value='2', line=3),
+            Token(name='COMMA', value=',', line=3),
+            Token(name='KEY', value='t', line=4),
+            Token(name='COLON', value=':', line=4),
+            Token(name='TRUE', value='true', line=4),
+            Token(name='COMMA', value=',', line=4),
+            Token(name='KEY', value='f', line=5),
+            Token(name='COLON', value=':', line=5),
+            Token(name='FALSE', value='false', line=5),
+            Token(name='COMMA', value=',', line=5),
+            Token(name='KEY', value='n', line=6),
+            Token(name='COLON', value=':', line=6),
+            Token(name='NULL', value='null', line=6),
+            Token(name='COMMA', value=',', line=6),
+            Token(name='KEY', value='list', line=7),
+            Token(name='COLON', value=':', line=7),
+            Token(name='OPEN_SQBRACE', value='[', line=7),
+            Token(name='STRING', value='text', line=8),
+            Token(name='COMMA', value=',', line=8),
+            Token(name='NUMBER', value='1', line=8),
+            Token(name='COMMA', value=',', line=8),
+            Token(name='TRUE', value='true', line=8),
+            Token(name='COMMA', value=',', line=8),
+            Token(name='OPEN_CURLYBRACE', value='{', line=8),
+            Token(name='KEY', value='nested', line=8),
+            Token(name='COLON', value=':', line=8),
+            Token(name='STRING', value='inside', line=8),
+            Token(name='CLOSE_CURLYBRACE', value='}', line=8),
+            Token(name='CLOSE_SQBRACE', value=']', line=9),
+            Token(name='COMMA', value=',', line=9),
+            Token(name='KEY', value='la"st\\', line=10),
+            Token(name='COLON', value=':', line=10),
+            Token(name='NUMBER', value='-1234.123123e+123123', line=10),
+            Token(name='CLOSE_CURLYBRACE', value='}', line=11)
+        ]
+        self.assertEqual(list(tokenize(text)), output)
+
+    def test_keywords(self):
+        text = '{"key,[1]": "{value1}"}'
+        output = [
+            Token(name='OPEN_CURLYBRACE', value='{', line=1),
+            Token(name='KEY', value='key,[1]', line=1),
+            Token(name='COLON', value=':', line=1),
+            Token(name='STRING', value='{value1}', line=1),
+            Token(name='CLOSE_CURLYBRACE', value='}', line=1)
+        ]
+        self.assertEqual(list(tokenize(text)), output)
+
+    def test_generated(self):
+        text = '{\n'
+        output = [Token(name='OPEN_CURLYBRACE', value='{', line=1)]
+        n = 10000
+        for x in range(n):
+            text += f'"key{x}": "value{x}",\n'
+            output += [
+                Token(name='KEY', value=f'key{x}', line=x+2),
+                Token(name='COLON', value=':', line=x+2),
+                Token(name='STRING', value=f'value{x}', line=x+2),
+                Token(name='COMMA', value=',', line=x+2),
+            ]
+
+        text += '"last": "value"}'
+        output += [
+            Token(name='KEY', value='last', line=n+2),
+            Token(name='COLON', value=':', line=n+2),
+            Token(name='STRING', value='value', line=n+2),
+            Token(name='CLOSE_CURLYBRACE', value='}', line=n+2)
+        ]
+        self.assertEqual(list(tokenize(text)), output)
 
 
 if __name__ == '__main__':
--- tokenizer.py
+++ tokenizer.py
@@ -1,21 +1,8 @@
 import re
+from collections import namedtuple
 
 
-patterns = [
-    ('OPEN_CURLYBRACE', r'{'),
-    ('KEY', r'(?<=")[\w\"\\\/\b\f\n\r\t]+(?=":)'),
-    ('COLON', r':'),
-    ('COMMA', r','),
-    ('TRUE', r'true(?=(,)|(\s*})|(\s*\]))'),
-    ('FALSE', r'false(?=(,)|(\s*})|(\s*\]))'),
-    ('NULL', r'null(?=(,)|(\s*})|(\s*\]))'),
-    ('NUMBER', r'-?(0|([1-9]\d*))(\.\d+)?([eE][-+]?\d+)?'
-        '(?=(,)|(\s*})|(\s*\]))'),
-    ('STRING', r'(?<=")[\w\"\\\/\b\f\n\r\t]+(?=(",)|("\s*})|("\s*\]))'),
-    ('OPEN_SQBRACE', r'\['),
-    ('CLOSE_SQBRACE', r'\]'),
-    ('CLOSE_CURLYBRACE', r'}'),
-]
+Token = namedtuple('Token', ['name', 'value', 'line'])
 
 
 def tokenize(text):
@@ -28,14 +15,36 @@ def tokenize(text):
     Returns:
         A list of tuples with token name and value
 
-    Examples:
+    Example:
     >>> for token in tokenize('{"key": "value"}'):
     ...     print(token)
-    ('OPEN_CURLYBRACE', '{')
-    ('KEY', 'key')
-    ('COLON', ':')
-    ('STRING', 'value')
-    ('CLOSE_CURLYBRACE', '}')
+    Token(name='OPEN_CURLYBRACE', value='{', line=1)
+    Token(name='KEY', value='key', line=1)
+    Token(name='COLON', value=':', line=1)
+    Token(name='STRING', value='value', line=1)
+    Token(name='CLOSE_CURLYBRACE', value='}', line=1)
     """
+    patterns = [
+        ('KEY', r'(?<=")[\w\"\\\/\b\f\r\t,\[\]{}]+(?=":)'),
+        ('TRUE', r'true(?=(,)|(\s*})|(\s*\]))'),
+        ('FALSE', r'false(?=(,)|(\s*})|(\s*\]))'),
+        ('NULL', r'null(?=(,)|(\s*})|(\s*\]))'),
+        ('NUMBER', r'-?(0|([1-9]\d*))(\.\d+)?([eE][-+]?\d+)?'
+            '(?=(,)|(\s*})|(\s*\]))'),
+        ('STRING', r'(?<=")[\w\"\\\/\b\f\r\t,\[\]{}]+'
+            '(?=(",)|("\s*})|("\s*\]))'),
+        ('COLON', r':'),
+        ('COMMA', r','),
+        ('NEWLINE', r'\n'),
+        ('OPEN_SQBRACE', r'\['),
+        ('CLOSE_SQBRACE', r'\]'),
+        ('OPEN_CURLYBRACE', r'{'),
+        ('CLOSE_CURLYBRACE', r'}'),
+    ]
+    line = 1
     regex = re.compile('|'.join([f'(?P<{x}>{y})' for x, y in patterns]))
-    return [(x.lastgroup, x.group(x.lastgroup)) for x in regex.finditer(text)]
+    for x in regex.finditer(text):
+        if x.lastgroup == 'NEWLINE':
+            line += 1
+        else:
+            yield Token(x.lastgroup, x.group(x.lastgroup), line)
