b0cb23c8cfa8a0927e6a1da1719fba6320f91b5c
arsh23 <programmer.arsh@gmail.com>
2018-06-25 03:31:25 +0530

    parser v0.3

--- parser.py
+++ parser.py
@@ -4,13 +4,21 @@ from collections import namedtuple
 Result = namedtuple('Result', ['success', 'result', 'input'])
 
 
-def char(char):
-    def parser(inp):
-        nonlocal char
+class Parser():
+    pass
+
+
+class Char(Parser):
+
+    def __init__(self, char):
+        self.char = char
+
+    def __call__(self, inp):
         if not isinstance(inp, str) or inp == "":
             return Result(False, 'No string found to parse', '')
-        if inp[0] == char:
-            return Result(True, char, inp[1:])
+        if inp[0] == self.char:
+            return Result(True, self.char, inp[1:])
         else:
-            return Result(False, f'Expecting "{char}" but got "{inp[0]}"', '')
-    return parser
+            return Result(
+                False, f'Expecting "{self.char}" but got "{inp[0]}"', ''
+            )
