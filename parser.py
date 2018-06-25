19791ab86080891327ecd5e92ac306b23844ae70
arsh23 <programmer.arsh@gmail.com>
2018-06-25 22:26:23 +0530

    parser v0.6

--- parser.py
+++ parser.py
@@ -1,4 +1,6 @@
-from collections import namedtuple
+from collections import namedtuple, Callable
+from operator import add, or_
+from functools import reduce
 
 
 Result = namedtuple('Result', ['success', 'result', 'input'])
@@ -31,6 +33,15 @@ class Parser():
             return parser2(inp)
         return Parser(parse_func)
 
+    def map(self, modify_fn):
+        if isinstance(modify_fn, Callable):
+            parse_fn = self.parse
+
+            def new_fn(inp):
+                return modify_fn(parse_fn(inp))
+            self.parse = new_fn
+        return self
+
 
 class Char(Parser):
 
@@ -40,9 +51,38 @@ class Char(Parser):
     def parse(self, inp):
         if not isinstance(inp, str) or inp == "":
             return Result(False, 'No string found to parse', '')
-        if inp[0] == self.char:
+        if inp[0] == str(self.char):
             return Result(True, self.char, inp[1:])
         else:
             return Result(
                 False, f'Expected "{self.char}" but got "{inp[0]}"', ''
             )
+
+
+def choice(list_of_parsers):
+    return reduce(or_, list_of_parsers)
+
+
+def Any(list_of_chars):
+    return choice(Char(x) for x in list_of_chars)
+
+
+def sequence(list_of_parsers):
+    return reduce(add, list_of_parsers)
+
+
+def flatten(r):
+    def flatten_list(l):
+        if isinstance(l, list):
+            return [x for inner in l for x in flatten_list(inner)]
+        else:
+            return [l]
+
+    if r.success:
+        return Result(True, flatten_list(r.result), r.input)
+    else:
+        return r
+
+
+def Seq(list_of_chars):
+    return sequence(Char(x) for x in list_of_chars).map(flatten)
