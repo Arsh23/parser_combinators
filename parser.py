3162a1eee8e21b955932164ae43743519b7366ba
arsh23 <programmer.arsh@gmail.com>
2018-06-26 02:29:58 +0530

    parser v0.8

--- parser.py
+++ parser.py
@@ -32,13 +32,17 @@ class Parser():
             return parser2(inp)
         return Parser(parse_func)
 
+    def __invert__(self):
+        def always_true(res):
+            if not res.success:
+                return Result(True, [], res.input)
+            return res
+        return self.apply(always_true)
+
     def apply(self, modify_fn):
         if isinstance(modify_fn, Callable):
             parse_fn = self.parse
-
-            def new_fn(inp):
-                return modify_fn(parse_fn(inp))
-            self.parse = new_fn
+            return Parser(lambda inp: modify_fn(parse_fn(inp)))
         return self
 
 
@@ -53,9 +57,8 @@ class Char(Parser):
         if inp[0] == str(self.char):
             return Result(True, self.char, inp[1:])
         else:
-            return Result(
-                False, f'Expected "{self.char}" but got "{inp[0]}"', ''
-            )
+            error_msg = f'Expected "{self.char}" but got "{inp[0]}"'
+            return Result(False, error_msg, inp)
 
 
 def sequence(list_of_parsers):
@@ -69,3 +72,21 @@ def sequence(list_of_parsers):
 choice = lambda list_of_parsers: reduce(lambda x, y: x | y, list_of_parsers)
 Any = lambda list_of_chars: choice(Char(x) for x in list_of_chars)
 Seq = lambda list_of_chars: sequence(Char(x) for x in list_of_chars)
+
+
+def ZeroOrMore(parser):
+    def new_parser(inp):
+        res = parser(inp)
+        if not res.success:
+            return Result(True, [], inp)
+        next_res = new_parser(res.input)
+        return Result(True, [res.result] + next_res.result, next_res.input)
+    return Parser(new_parser)
+
+
+def OneOrMore(parser):
+    def flatten_result(res):
+        if res.success and isinstance(res.result[1], list):
+            return Result(True, [res.result[0]] + res.result[1], res.input)
+        return res
+    return (parser + ZeroOrMore(parser)).apply(flatten_result)
